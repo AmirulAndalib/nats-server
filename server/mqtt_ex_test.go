745ccd12108b4a9ad5cdd29157c2902ccc7cf298
Lev Brouk <lev@synadia.com>
2024-01-16 22:51:59 -0800

    removed ReportAllocs() - confuses the bench action diffs

--- server/mqtt_ex_test.go
+++ server/mqtt_ex_test.go
@@ -410,7 +410,10 @@ func (r MQTTBenchmarkResult) report(b *testing.B) {
 		nsOp := float64(ns) / float64(r.Ops)
 		b.ReportMetric(nsOp/1000000, unit+"_ms/op")
 	}
-	b.ReportAllocs()
+	
+	// Diable ReportAllocs() since it confuses the github benchmarking action
+	// with the noise.
+	// b.ReportAllocs()
 }
 
 func mqttNewBenchEx(b *testing.B) *mqttBenchContext {
